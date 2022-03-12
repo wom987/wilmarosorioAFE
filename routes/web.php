6bb13c9f9029913d82f5f493898350b8b83a7008
wom987 <45610462+wom987@users.noreply.github.com>
2022-03-12 17:56:00 -0600

    redirecting route from controller

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/product', function () {
-    return view('product');
-});
+Route::get('/product', "ProductController@product");
