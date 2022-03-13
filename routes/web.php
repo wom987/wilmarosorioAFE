df15961fbf6451a79668b46f82ed2da1381fda98
wom987 <45610462+wom987@users.noreply.github.com>
2022-03-12 18:56:33 -0600

    bootstrap and vue added to proyect

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/product', "ProductController@product");
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
