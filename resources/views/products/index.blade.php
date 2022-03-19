cb8f3c2d7bde1d7e3b6e0944c441b26ea360c0f2
wom987 <45610462+wom987@users.noreply.github.com>
2022-03-19 17:25:51 -0600

    product table added

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -2,16 +2,19 @@
 @section('css')
 @endsection
 @section('content')
-<h1>Productos</h1>
-
-@foreach ($products as $p)
-    <ul>{{ $p }}</ul>
-@endforeach
-<h1>Proveedores</h1>
-
-@foreach ($proveedores as $pr)
-    <ul>{{ $pr }}</ul>
-@endforeach
+    <h1>Productos</h1>
+    <h2 class="text-center mb-5">Administracion de productos</h2>
+    <div class="col-md-10 mx-auto bg-white p-3">
+        <table class="table">
+            <thead class="bg-primary text-light">
+                <tr>
+                    <th scole="col">Codigo</th>
+                    <th scole="col">Nombre</th>
+                    <th scole="col">Acciones</th>
+                </tr>
+            </thead>
+        </table>
+    </div>
 @endsection
 @section('js')
 @endsection
