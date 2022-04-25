8990950c19d03d1bd6ec333713374b4d89aea158
wom987 <45610462+wom987@users.noreply.github.com>
2022-04-24 20:34:54 -0600

    filter products by user at controller added

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -14,7 +14,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        $products = Product::all();
+        $products = auth()->user()->products;
         return view('products.index', compact('products'));
     }
 
