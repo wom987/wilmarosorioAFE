95e7d4f4db54894016b99b5e78448d0513843857
wom987 <45610462+wom987@users.noreply.github.com>
2022-04-24 20:33:37 -0600

    user->products relationship added

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    //relations has many added
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
